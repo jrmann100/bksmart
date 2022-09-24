98524568113482ff175d8cf4fbd2b79d924bd000
Jordan Mann <jordaloo@gmail.com>
2022-09-24 18:43:49 -0400

    overhaul suggestions with folder scanning

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
 // todo: cross-browser support
 
+function pluralS(count) {
+  // fixme
+  return count !== 1 ? "s" : "";
+}
+
 const delim = "";
 
 // suggestions are broken in manifest v3:
@@ -57,32 +62,98 @@ const getPath = async (root, bookmark) =>
 const getPathS = (path) => path.map((x) => x.title.split(",")[0]).join(delim);
 
 chrome.omnibox.setDefaultSuggestion({
-  description: "[folder] [subfolder] ... [bookmark]",
+  description: ["[folder]", "[subfolder]", "...", "[bookmark]"].join(delim),
 });
 
-chrome.omnibox.onInputChanged.addListener(async (input, suggest) => {
+// todo oninputstarted
+
+// todo test with s/
+// https://stackoverflow.com/a/7918944
+if (!String.prototype.encodeHTML) {
+  String.prototype.encodeHTML = function () {
+    return this.replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&apos;");
+  };
+}
+
+async function handleInput(input, suggest) {
   const root = (await chrome.bookmarks.getTree.callback())[0];
   const bksmart = find(root, "bksmart", bookmarkTypes.folder);
-  const matches = search(bksmart, input, bookmarkTypes.bookmark); // todo: path parsing
-  const suggestions = await Promise.all(
-    matches.map(async (match) => {
-      const path = await getPath(bksmart, match);
-      return {
-        description: `${path.map((x) => x.title.split(",")[0]).join(" > ")} (${
-          match.url
-        })`, // how to structure this function-wise?
-        content: getPathS(path),
-      };
-    })
-  );
-  suggest(suggestions);
-});
+  // const matches = search(bksmart, input, bookmarkTypes.bookmark); // todo: path parsing
+  // const suggestions = await Promise.all(
+  //   matches.map(async (match) => {
+  //     const path = await getPath(bksmart, match);
+  //     return {
+  //       description: `${path.map((x) => x.title.split(",")[0]).join(" > ")} (${
+  //         match.url
+  //       })`, // how to structure this function-wise?
+  //       content: getPathS(path),
+  //     };
+  //   })
+  // );
+  // suggest(suggestions);
+  const components = input.split(delim);
+  let node = bksmart;
+  for (
+    let i = 0, component = components[i];
+    i < components.length;
+    component = components[++i]
+  ) {
+    console.debug(node, component);
+    if (node.children !== undefined) {
+      node = node.children.find((child) => matches(child, component));
+      if (node === undefined) {
+        break;
+      }
+    } else {
+      break;
+    }
+  }
+  console.debug(input, node);
+  if (node === undefined) {
+    suggest([{ description: "No matches found.", content: "_" }]);
+  } else if (node.url !== undefined) {
+    const suggestion = [
+      {
+        description: `<dim>${input
+          .split(delim)
+          .slice(0, -1)
+          .join(delim)}</dim><match>${input
+          .split(delim)
+          .at(-1)}</match> <url>${node.url.encodeHTML()}</url>`,
+        content: [input, node.title].join(delim),
+      },
+    ];
+    console.debug("suggesting", suggestion);
+    suggest(suggestion);
+  } else {
+    const suggestions = node.children.map(
+      (child) => ({
+        description:
+          `<dim>${input}</dim><match>${child.title}</match> ` +
+          (child.url !== undefined
+            ? `<url>${child.url}</url>`
+            : `[${child.children.length} item${pluralS(
+                child.children.length
+              )}]`),
+        content: [input, child.title].join(delim),
+      })
+      // ({ description: child.title, content: child.title })
+    );
+    console.debug("suggesting", suggestions);
+    suggest(suggestions);
+  }
+}
+chrome.omnibox.onInputChanged.addListener(handleInput);
 
 chrome.omnibox.onInputEntered.addListener(async (input, disposition) => {
   const root = (await chrome.bookmarks.getTree.callback())[0];
   const bksmart = find(root, "bksmart", bookmarkTypes.folder);
   let url;
-  if (input === "") {
+  if (input === "" || input === "_") {
     url = `chrome://bookmarks/?id=${bksmart.id}`;
   } else {
     const components = input.split(delim);
