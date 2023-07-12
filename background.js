3d46c621be05dd6c2173ac09dd4526f298d84040
Jordan Mann <jordaloo@gmail.com>
2023-07-12 13:14:01 -0400

    rewrite and bump version

--- background.js
+++ background.js
@@ -1,178 +1,105 @@
-// todo: cross-browser support
-
-function pluralS(count) {
-  // fixme
-  return count !== 1 ? "s" : "";
-}
-
 const delim = "";
 
-const find = (bookmark, title, type = bookmarkTypes.bookmark) => {
-  const results = search(bookmark, title, type);
-  if (results.length > 1)
-    throw Error(`Found multiple matches for search ${title}.`);
-  return results[0];
-};
-
-const bookmarkTypes = {
-  folder: 1,
-  bookmark: 2,
-};
-
-const matchType = (type, bookmark) => {
-  if (type === bookmarkTypes.folder) return bookmark.url === undefined;
-  else if (type === bookmarkTypes.bookmark) return bookmark.url !== undefined;
-};
-
-const matches = (bookmark, title) => {
-  return bookmark.title?.split(",").includes(title);
-};
-// this is both great and slow because it scans the entire tree.
-const search = (bookmark, title, type = bookmarkTypes.bookmark) => {
-  // should we support comma separation? is it helpful?
-  if (matchType(type, bookmark) && matches(bookmark, title)) return [bookmark];
-  else if (bookmark.children)
-    return bookmark.children
-      ?.map((child) => search(child, title, type))
-      .flat(1)
-      .filter((result) => result !== null);
-  else return null;
-};
-
-const getPath = async (root, bookmark) =>
-  bookmark.parentId === root.id
-    ? [bookmark]
-    : [
-        ...(await getPath(
-          root,
-          (
-            await chrome.bookmarks.get(bookmark.parentId)
-          )[0]
-        )),
-        bookmark,
-      ];
-
-const getPathS = (path) => path.map((x) => x.title.split(",")[0]).join(delim);
-
-chrome.omnibox.setDefaultSuggestion({
-  description: ["[folder]", "[subfolder]", "...", "[bookmark]"].join(delim),
-});
-
-// todo oninputstarted
-
-// todo test with s/
-// https://stackoverflow.com/a/7918944
-if (!String.prototype.encodeHTML) {
-  String.prototype.encodeHTML = function () {
-    return this.replace(/&/g, "&amp;")
-      .replace(/</g, "&lt;")
-      .replace(/>/g, "&gt;")
-      .replace(/"/g, "&quot;")
-      .replace(/'/g, "&apos;");
-  };
+// if the node has children, it's a folder.
+// otherwise it's a leaf (bookmark).
+function isFolder(node) {
+  return node.children !== undefined;
 }
 
-async function handleInput(input, suggest) {
+// suggestions are encoded in XML, but the content of the suggestions
+// (URLs, paths) may contain some XML-unsafe characters.
+function escapeXML(string) {
+  return string
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll("&", "&amp;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&apos;");
+}
+
+// add a suffix to pluralize a word if the
+// item count is not exactly 1; as in,
+// 0 items, 1 item, 2 items
+function pluralize(count, suffix = "s") {
+  return count !== 1 ? suffix : "";
+}
+
+// perform a queue-based breadth-first search for the bksmart node.
+const getBksmart = async () => {
   const root = (await chrome.bookmarks.getTree())[0];
-  const bksmart = find(root, "bksmart", bookmarkTypes.folder);
-  // const matches = search(bksmart, input, bookmarkTypes.bookmark); // todo: path parsing
-  // const suggestions = await Promise.all(
-  //   matches.map(async (match) => {
-  //     const path = await getPath(bksmart, match);
-  //     return {
-  //       description: `${path.map((x) => x.title.split(",")[0]).join(" > ")} (${
-  //         match.url
-  //       })`, // how to structure this function-wise?
-  //       content: getPathS(path),
-  //     };
-  //   })
-  // );
-  // suggest(suggestions);
+  const queue = [root];
+  for (let node = queue.shift(); node !== undefined; node = queue.shift()) {
+    if (isFolder(node) && node.title === "bksmart") {
+      return node;
+    }
+    if (node.children) {
+      queue.push(...node.children);
+    }
+  }
+  throw new Error("Could not find bksmart folder");
+};
+
+// trace a path input within the tree starting at the root (bksmart).
+// return the deepest node which was reached,
+// alongside the total number of nodes traversed.
+
+function search(bksmart, input) {
   const components = input.split(delim);
   let node = bksmart;
+  let i, component;
   for (
-    let i = 0, component = components[i];
+    i = 0, component = components[i];
     i < components.length;
-    component = components[++i]
+    i++, component = components[i]
   ) {
-    console.debug(node, component);
-    if (node.children !== undefined) {
-      node = node.children.find((child) => matches(child, component));
-      if (node === undefined) {
-        break;
+    if (isFolder(node)) {
+      // can we go down a level to the child which matches this component?
+      const next = node.children?.find((child) => child.title === component);
+      if (next === undefined) {
+        // no, there is no child which matches. fail at the parent.
+        return [node, i];
       }
+      node = next;
+      continue;
     } else {
-      break;
+      // we can't go down a level because this is a leaf. fail at the leaf.
+      return [node, i];
     }
   }
-  console.debug(input, node);
-  if (node === undefined) {
-    suggest([{ description: "No matches found.", content: "_" }]);
-  } else if (node.url !== undefined) {
-    const suggestion = [
-      {
-        description: `<dim>${input
-          .split(delim)
-          .slice(0, -1)
-          .join(delim)}</dim><match>${input
-          .split(delim)
-          .at(-1)}</match> <url>${node.url.encodeHTML()}</url>`,
-        content: [input, node.title].join(delim),
-      },
-    ];
-    console.debug("suggesting", suggestion);
-    suggest(suggestion);
-  } else {
-    const suggestions = node.children.map(
-      (child) => ({
-        description:
-          `<dim>${input}</dim><match>${child.title}</match> ` +
-          (child.url !== undefined
-            ? `<url>${child.url}</url>`
-            : `[${child.children.length} item${pluralS(
-                child.children.length
-              )}]`),
-        content: [input, child.title].join(delim),
-      })
-      // ({ description: child.title, content: child.title })
-    );
-    console.debug("suggesting", suggestions);
-    suggest(suggestions);
-  }
+  // success - we have traced the entire path; i === input.length.
+  return [node, i];
 }
-chrome.omnibox.onInputChanged.addListener(handleInput);
 
-chrome.omnibox.onInputEntered.addListener(async (input, disposition) => {
-  const root = (await chrome.bookmarks.getTree())[0];
-  const bksmart = find(root, "bksmart", bookmarkTypes.folder);
-  let url;
-  if (input === "" || input === "_") {
-    url = `chrome://bookmarks/?id=${bksmart.id}`;
-  } else {
-    const components = input.split(delim);
-    let node = bksmart;
-    for (
-      let i = 0, component = components[i];
-      i < components.length;
-      component = components[++i]
-    ) {
-      console.debug(node, component);
-      if (node.children !== undefined) {
-        node = node.children.find((child) => matches(child, component));
-        if (node === undefined) {
-          break;
-        }
-      } else {
-        break;
-      }
-    }
-    if (node === undefined) {
-      console.warn("could not match query:", input);
-      return;
-    }
-    url = node.url ?? node?.children.find((child) => matches(child, "_")).url;
-  }
-  if (url === undefined) return;
+chrome.omnibox.onInputChanged.addListener(async (input, suggest) => {
+  const bksmart = await getBksmart();
+  const [node, matched] = search(bksmart, input);
+
+  // what is the path which describes the parent of the nodes we want to suggest?
+  // if our node is a folder, then the path is the input itself.
+  // example: tree is a -> b -> (1,2,3), input is "ab"; then we will suggest ab>1, ab>2, ab>3
+  // where "ab" is the closest valid folder
+  // if the node is a leaf, then the path is the parent of the leaf.
+  // example: tree is a -> b -> (1,2,3), input is "ab1"; then we will suggest ab>1
+  // where "ab" is the closest valid folder
+  const closestValidFolder = input.slice(0, matched - !isFolder(node));
+  suggest(
+    (isFolder(node) ? node.children : [node]).map((child) => ({
+      description:
+        `<dim>${escapeXML(closestValidFolder)}</dim><match>${escapeXML(
+          child.title
+        )}</match> ` +
+        (child.url !== undefined
+          ? `<url>${escapeXML(child.url)}</url>`
+          : `[${child.children.length} item${pluralize(
+              child.children.length
+            )}]`),
+      content: [closestValidFolder, child.title].join(delim),
+    }))
+  );
+});
+
+// open the URL correctly depending on the disposition.
+function open(url, disposition) {
   switch (disposition) {
     case "currentTab":
       chrome.tabs.update({ url });
@@ -184,4 +111,48 @@ chrome.omnibox.onInputEntered.addListener(async (input, disposition) => {
       chrome.tabs.create({ url, active: false });
       break;
   }
+}
+
+// get a URL to this node in the bookmarks index.
+function folderURL(node) {
+  return `chrome://bookmarks/?id=${node.id}`;
+}
+
+chrome.omnibox.setDefaultSuggestion({
+  description: "[folder][subfolder...][bookmark]",
+});
+
+chrome.omnibox.onInputEntered.addListener(async (input, disposition) => {
+  const bksmart = await getBksmart();
+  let url;
+  determineURL: {
+    // if the input is whitespace or an underscore, go bksmart in the bookmarks index.
+    if (input === "" || input === "_") {
+      url = folderURL(bksmart);
+      break determineURL;
+    }
+    const [node, matched] = search(bksmart, input);
+    // if we could not match the whole path, then fail silently.
+    if (matched < input.length) {
+      return;
+    }
+    // if this is a leaf, then the URL is the leaf's URL.
+    if (!isFolder(node)) {
+      url = node.url;
+      break determineURL;
+    }
+    // else this is a folder.
+    // if there is a child with an underscore as the title for this folder,
+    // then that child's URL is the URL which represents the folder.
+    const underscored = node.children.find(
+      (child) => !isFolder(child) && child.title === "_"
+    );
+    if (underscored !== undefined) {
+      url = underscored.url;
+      break determineURL;
+    }
+    // otherwise, just open the folder in the bookmarks index.
+    url = folderURL(node);
+  }
+  open(url, disposition);
 });
